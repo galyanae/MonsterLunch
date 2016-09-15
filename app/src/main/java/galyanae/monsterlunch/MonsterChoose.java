ef003151bf07b5973faede6335eb1d7bddbcf60e
galyanae <galyanae@gmail.com>
2016-09-15 11:58:23 +0300

    Monster choose

--- app/src/main/java/galyanae/monsterlunch/MonsterChoose.java
+++ app/src/main/java/galyanae/monsterlunch/MonsterChoose.java
@@ -1,13 +1,36 @@
 package galyanae.monsterlunch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MonsterChoose extends AppCompatActivity {
 
+
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monster_choose);
+    }
+
+    public void sweet(View view) {
+        intent = new Intent(this, MainActivity.class);
+        intent.putExtra("name","Sweety");
+        startActivity(intent);
+    }
+
+
+    public void lux(View view) {
+        intent = new Intent(this, MainActivity.class);
+        intent.putExtra("name","Luxor");
+        startActivity(intent);
+    }
+
+    public void health(View view) {
+        intent = new Intent(this, MainActivity.class);
+        intent.putExtra("name","Yoggy");
+        startActivity(intent);
     }
 }
